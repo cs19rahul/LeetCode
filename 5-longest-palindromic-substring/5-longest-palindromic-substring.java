a09600f0cd983a04cdd8ef0331e7a6d60072a624
cs19rahul <79753953+cs19rahul@users.noreply.github.com>
2022-05-26 23:56:17 +0530

    Time: 687 ms (11.34%), Space: 79.8 MB (23.72%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        
+        for(boolean[] i:dp){
+            Arrays.fill(i,false);
+        }
+        form(dp,s);
+        // for(int i = 0 ;i<dp.length;i++){
+            // for(int j = 0 ;j<dp.length;j++){
+        //        System.out.print(dp[i][j]+" ");
+        //    }
+        //     System.out.println();
+        // }
+        boolean flag = false;
+        int k=0 ,l=s.length()-1;
+        for(int g = dp.length-1;g>=0;g--){
+            for(int i=0,j=g;j<dp.length && flag==false;j++,i++){
+                if(dp[i][j]==true){
+                    k = i;
+                    l = j ;
+                  
+                    flag = true;
+                    break;
+                }
+            }
+            
+        }
+        l = l+1;
+        
+        return s.substring(k,l);
+    }
+   void  form(boolean[][]dp , String s){
+        for(int g = 0 ; g <s.length() ; g++){
+             for(int i =0 , j=g ; j<s.length() ; j++ , i++) {
+                 if(g==0) dp[i][j]= true;
+                 else if(g==1) {
+                     if(s.charAt(i)==s.charAt(j)) dp[i][j]= true;
+                     else dp[i][j]= false;
+                     
+                 }
+                 else{
+                     if(s.charAt(i)==s.charAt(j) &&  dp[i+1][j-1]==true){
+                         dp[i][j]= true;
+                         
+                     }
+                     else  dp[i][j] = false;
+                 }
+             }
+        }
+    }
+}
