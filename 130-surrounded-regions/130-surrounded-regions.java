36800444327d9d73a862c36ba3471f901aa3a43a
cs19rahul <79753953+cs19rahul@users.noreply.github.com>
2022-07-15 19:27:30 +0530

    Time: 205 ms (5.19%), Space: 55 MB (7.06%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void solve(char[][] board) {
+        System.out.println(board.length + " "+board[0].length);
+        for(int i = 0 ; i < board.length ; i++){
+            for(int j = 0 ; j< board[0].length ;j++){
+                if((i==0 || i == board.length-1 ||j==0||j==board[0].length-1) && board[i][j]=='O' ){
+                    call(board,i,j);
+                }
+            }
+        }
+        
+         for(int i = 0 ; i < board.length ; i++ ){
+            for(int j = 0 ; j < board[0].length ;j++){
+                System.out.print(board[i][j]+" ");
+            }
+         System.out.println();}
+        
+        
+//         update
+        for(int i = 0 ; i < board.length ; i++ ){
+            for(int j = 0 ; j < board[0].length ;j++){
+                
+                if(board[i][j]=='O') board[i][j] = 'X' ;
+                else if (board[i][j]=='2') board[i][j] = 'O';
+            }
+        }
+    }
+    
+    
+    
+    
+    
+        void call(char[][]g ,int i  ,int j){
+            if(i<0 || i>g.length-1 || j>g[0].length-1 || j<0 || g[i][j]=='X' || g[i][j]=='2'){
+            return ;}
+        
+            if(g[i][j]=='O')
+        g[i][j] = '2';
+            
+            
+  call(g,i-1 ,j );
+   call(g,i,j+1);
+   call(g,i+1,j);
+   call(g,i,j-1);
+    return ;
+        }
+    
+}
