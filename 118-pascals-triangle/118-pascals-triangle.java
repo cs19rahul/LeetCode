ccb38e0c78a99ced8414cf84951afba497975a31
cs19rahul <79753953+cs19rahul@users.noreply.github.com>
2022-07-23 17:07:23 +0530

    Time: 3 ms (72.34%), Space: 42.3 MB (8.93%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -3,17 +3,17 @@ class Solution {
         	
 		
 	List<List<Integer>> res = new ArrayList<>();
-	List<Integer> curr ,pre = null;
+	// List<Integer> curr ,pre = null;
 	for(int i = 0;i<n;i++) {
-		curr = new ArrayList<>();
+		List<Integer>curr = new ArrayList<>();
 		for(int j = 0;j<=i;j++) {
 			if(j==0||j==i) {
 				curr.add(1);
 			}
 			else
-			curr.add(pre.get(j-1)+pre.get(j));
+			curr.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
 		}
-		pre = curr;
+		// pre = curr;
 		res.add(curr);
 	}
 	System.out.println(res);
