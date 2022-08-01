6ca26d8e4f5b7ddb53fd04209485c669a2f947cd
cs19rahul <79753953+cs19rahul@users.noreply.github.com>
2022-08-01 23:25:18 +0530

    Time: 1 ms (55.39%), Space: 41.4 MB (17.36%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        return call(m,n);
+    }
+    public int call(int m ,int n){
+      int[][] dp = new int[m][n];
+        dp[0][0]=0;
+    for(int i = 0;i<m;i++){
+        for(int j = 0;j<n;j++){
+            if(i==0  && j== 0 )dp[i][j]=1;
+            else {
+            int d = 0,r=0;
+            if(i>0) {
+                 d =dp[i-1][j];
+                
+            }
+            if(j>0){
+                r = dp[i][j-1];
+            }
+        dp[i][j]= d+r;
+        }
+        
+    }
+    }
+        return dp[m-1][n-1];
+    }
+}
